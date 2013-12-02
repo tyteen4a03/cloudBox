7b7ae8db9aa3646548303ec122f84895528635f6
tyteen4a03 <tyteen4a03@3pointstudio.com>
2013-12-02 03:36:30 +0100

    A DatabaseClient.

--- cloudbox/database/client/protocol.py
+++ cloudbox/database/client/protocol.py
@@ -2,3 +2,41 @@
 # cloudBox is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSE" file in the "docs" folder of the
 # cloudBox Package.
+
+from twisted.internet.protocol import Protocol
+
+from cloudbox.common.gpp import MSGPackPacketProcessor
+from cloudbox.common.logger import Logger
+from cloudbox.common.mixins import CloudBoxProtocolMixin
+
+
+class DatabaseClientProtocol(Protocol, CloudBoxProtocolMixin):
+    """
+    I am a Protocol for the World Server.
+    """
+
+    def sendDirectQuery(self):
+
+    ### Twisted-related functions ###
+
+    def makeConnection(self, transport):
+        self.logger = Logger()
+        self.transport = transport
+        self.ready = False  # Set to False to prevent new connections
+        self.gpp = MSGPackPacketProcessor(self, self.factory.handlers)
+        self.sendHandshake()
+
+    def connectionMade(self):
+        self.ready = True
+        self.factory.instance = self
+
+    def dataReceived(self, data):
+        self.gpp.feed(data)
+        self.gpp.parseFirstPacket()
+
+    def connectionLost(self, reason):
+        self.logger.error("Connection to Hub Server lost: {reason}".format(reason=reason))
+
+    @property
+    def remoteServerType(self):
+        return self.factory.remoteServerType
