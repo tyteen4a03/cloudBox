822e7464a182dc5396c330060c3c1aa82bd7a700
tyteen4a03 <tyteen4a03@3pointstudio.com>
2014-01-19 16:21:02 +0100

    Happy 2014!

--- cloudbox/hub/world/protocol.py
+++ cloudbox/hub/world/protocol.py
@@ -1,9 +1,7 @@
-# cloudBox is copyright 2012 - 2013 the cloudBox team.
+# cloudBox is copyright 2012 - 2014 the cloudBox team.
 # cloudBox is licensed under the BSD 2-Clause modified License.
 # To view more details, please see the "LICENSE" file in the "docs" folder of the
 # cloudBox Package.
-
-import logging
 
 from twisted.internet.protocol import Protocol
 
