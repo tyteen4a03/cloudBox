2670dc686a3ba4d207f6de3cd3df130c2ab5c41e
tyteen4a03 <tyteen4a03@3pointstudio.com>
2014-07-07 01:44:28 +0800

    The first of tests

---
+++ tests/common/test_gpp.py
@@ -0,0 +1,11 @@
+# cloudBox is copyright 2012 - 2014 the cloudBox team.
+# cloudBox is licensed under the BSD 2-Clause modified License.
+# To view more details, please see the "LICENSE" file in the "docs" folder of the
+# cloudBox Package.
+
+import unittest
+
+class TestGPP(unittest.TestCase):
+
+if __name__ == '__main__':
+    unittest.main()
